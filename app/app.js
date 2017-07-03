67d2e8e5fdc3530b9067a6b4639fa203d61934f1
Kouemapoh, Willy S <willy.s.kouemapoh@accenture.com>
2017-07-03 15:19:16 +0400

    filter resume integration

--- app/app.js
+++ app/app.js
@@ -4,17 +4,35 @@ var DEFAULT_JOB_POSITION = "Java Developer"
 //All level
 var DEFAULT_LEVEL = 6
 var DEFAULT_LOCATION = "Mauritius"
+var GCE_URL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAyyg155IU-yqORUYfe8-4_87Q8hFyv75M&cx=016927734479450611209:c_wpluobuuq&cr=countryUS&q=%22"
 
 // app components
 angular.module('pickmeHereApp', [
-  'ngRoute'
-]).controller('HomeCtrl', ['$scope', function ($scope) {
-
-  $scope.name = "testing";
+  'ngRoute',
+  'ngSanitize'
+]).controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
 
   $scope.$watch('filter.jobposition', function (newVal, oldVal) {
     if (newVal) {
+      GCE_URL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAyyg155IU-yqORUYfe8-4_87Q8hFyv75M&cx=016927734479450611209:c_wpluobuuq&cr=countryUS&q=%22"
+
+      GCE_URL = GCE_URL + newVal + "%22";
+     
       console.log(newVal);
+      // GET Request to google cse
+      $http({
+        method: 'GET',
+        url: GCE_URL
+      }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log(response.data.items);
+        $scope.resumes = response.data.items;
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.error(response);
+      });
     } else {
       console.log("bad");
     }
@@ -43,5 +61,4 @@ angular.module('pickmeHereApp', [
       console.log("bad");
     }
   });
-
 }]);
